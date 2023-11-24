56fa232dc4783e8ea6233f3b9a9cecab7fb2006b
Declan <declanprice1@gmail.com>
2023-11-24 14:30:05 +0000

    ui: toolbar added

--- ui/src/layout/Navbar.tsx
+++ ui/src/layout/Navbar.tsx
@@ -1,21 +1,41 @@
-import { createSignal, Show } from 'solid-js'
+import { createEffect, createSignal, Show } from 'solid-js'
 import { FiMenu } from 'solid-icons/fi'
 
 export const Navbar = () => {
     const [isNavBarOpen, setIsNavBarOpen] = createSignal(false)
+
+    const handleEsc = (event: any) => {
+        if (isNavBarOpen() && event.key === 'Escape') {
+            setIsNavBarOpen(false)
+        }
+    }
+
+    createEffect(() => {
+        if (!isNavBarOpen()) {
+            document.removeEventListener('keyup', handleEsc)
+        }
+    })
 
     return (
         <>
             <FiMenu
                 onClick={() => {
                     setIsNavBarOpen(true)
+
+                    document.addEventListener('keyup', handleEsc)
                 }}
             />
 
             <Show when={isNavBarOpen()}>
-                <div class="absolute left-0 bottom-0 w-full h-full bg-primary opacity-60">
-                    <div class="bg-primary w-[300px] flex flex-col h-full left-0 bottom-0">content</div>
+                <div class="absolute left-0 bottom-0 w-full h-full bg-primary w-[300px] flex flex-col h-full left-0 bottom-0 z-20">
+                    content
                 </div>
+                <div
+                    onClick={() => {
+                        setIsNavBarOpen(false)
+                    }}
+                    class="absolute right-0 bottom-0 w-full h-full flex-1 bg-primary opacity-60 z-10"
+                ></div>
             </Show>
         </>
     )
