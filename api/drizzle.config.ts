cf01925e1879e85e40beda81762e52120fb78729
Jed Burrows <jed@pop-os.localdomain>
2023-11-26 22:48:16 +0000

    fix: fixed db config file

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'drizzle-kit';
 
 export default {
   driver: 'pg',
-  schema: './src/global/database/schema',
+  schema: './src/database/schema',
   out: './drizzle',
   dbCredentials: {
     connectionString: 'postgres://postgres:postgres@localhost:5432',
